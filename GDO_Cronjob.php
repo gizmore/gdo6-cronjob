9185ffb63226d7a465cb94ae3e34da94535a8dc1
gizmore <gizmore@wechall.net>
2021-11-13 00:58:58 +0100

    6.10.6-r6177_cron

--- GDO_Cronjob.php
+++ GDO_Cronjob.php
@@ -4,11 +4,14 @@ namespace GDO\Cronjob;
 use GDO\Core\GDO;
 use GDO\DB\GDT_Name;
 use GDO\DB\GDT_AutoInc;
+use GDO\DB\GDT_String;
 use GDO\Date\GDT_DateTime;
 use GDO\DB\GDT_Checkbox;
 
 /**
  * This table holds info about the cronjob runnings.
+ * Scheduled entries are added on each run.
+ * 
  * @author gizmore
  * @version 6.10.6
  * @since 6.10.4
@@ -19,7 +22,7 @@ final class GDO_Cronjob extends GDO
     {
         return [
             GDT_AutoInc::make('cron_id'),
-            GDT_Name::make('cron_method')->notNull(),
+            GDT_String::make('cron_method')->ascii()->caseS()->notNull(),
             GDT_DateTime::make('cron_scheduled'),
         	GDT_DateTime::make('cron_started'),
         	GDT_DateTime::make('cron_finished'),
