d0448971901cc3b2f92d5b9ede525daff58b8474
gizmore <gizmore@wechall.net>
2021-12-23 18:53:44 +0100

    6.11.1-r6274

--- Method/Cronjob.php
+++ Method/Cronjob.php
@@ -8,6 +8,7 @@ use GDO\Form\GDT_Submit;
 use GDO\Form\MethodForm;
 use Exception;
 use GDO\UI\GDT_HTML;
+use GDO\User\GDO_User;
 
 /**
  * Development aid for testing cronjobs.
@@ -31,6 +32,9 @@ class Cronjob extends MethodForm
 	{
 		try
 		{
+			$user = GDO_User::current();
+			GDO_User::setCurrent(GDO_User::system());
+			
 			ob_start();
 			
 			echo "<pre>";
@@ -48,6 +52,7 @@ class Cronjob extends MethodForm
 		finally
 		{
 			ob_end_clean();
+			GDO_User::setCurrent($user);
 		}
 	}
 	
